b68176c05326c4d430a2ebf2fc91059d10d7f567
nathbalica <nathbalica@gmail.com>
2023-08-21 15:57:06 -0300

    add changes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { validateAuth } from "../middlewares/auth.middlewares.js";
 import { getTimeline, getUserById } from "../controllers/users.controller.js";
+import { searchUsersController } from "../controllers/users.controller.js";
 
 const usersRouter = Router();
 
 usersRouter.get("/user/:user_id", validateAuth, getUserById);
+usersRouter.get("/search", searchUsersController);
 usersRouter.get("/timeline", validateAuth, getTimeline);
 
 export default usersRouter;
