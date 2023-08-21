ffcd67a74e8e636b25abdfa42044d30fd3fc4822
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-08-21 13:41:01 -0300

    merge com codigo novo

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -10,8 +10,7 @@ import {
     removeLike,
     checkIfLiked,
 } from "../repositories/posts.repository.js";
-
-import { db } from "../database/database.connection.js";
+import { createPostHashtags } from "./hashtags.controller.js";
 
 export async function publish(req, res) {
     const { user_id } = res.locals;
@@ -20,42 +19,13 @@ export async function publish(req, res) {
         const post_id = await addPost(user_id, content, link);
         const hashtags = content.match(/#\w+/g);
         if (hashtags) {
-            const hashtagIds = await processHashtags(hashtags);
-            await addPostHashtags(post_id, hashtagIds);
+            await createPostHashtags(post_id, hashtags);
         }
         return res.sendStatus(201);
     } catch (error) {
         console.error("Erro ao publicar post:", error);
         res.sendStatus(500);
     }
-}
-
-async function processHashtags(hashtags) {
-    const normalizedHashtags = hashtags.map((hashtag) => hashtag.toLowerCase());
-    const existingHashtags = await getExistingHashtags(normalizedHashtags);
-    const newHashtags = await createNonExistingHashtags(
-        normalizedHashtags,
-        existingHashtags
-    );
-    return [...existingHashtags, ...newHashtags];
-}
-
-async function getExistingHashtags(hashtags) {
-    const query = `SELECT id, name FROM hashtags WHERE name IN ($1)`;
-    const result = await db.query(query, [hashtags]);
-    return result.rows.map((row) => row.id);
-}
-
-async function createNonExistingHashtags(hashtags, existingHashtags) {
-    const nonExistingHashtags = hashtags.filter(
-        (hashtag) => !existingHashtags.includes(hashtag)
-    );
-    const newHashtagIds = [];
-    for (const hashtag of nonExistingHashtags) {
-        const hashtagId = await createHashtag(hashtag);
-        newHashtagIds.push(hashtagId);
-    }
-    return newHashtagIds;
 }
 
 export async function editPostController(req, res) {
